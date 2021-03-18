06190ef8b5ca6bb76bdceaa85d2ebfcd2c352d7f
Nikita Zimin <nzeemin@gmail.com>
2021-03-18 19:04:43 +0300

    ScreenView context menu.

--- emulator/ScreenView.cpp
+++ emulator/ScreenView.cpp
@@ -11,6 +11,7 @@ NEMIGABTL. If not, see <http://www.gnu.org/licenses/>. */
 // ScreenView.cpp
 
 #include "stdafx.h"
+#include <windowsx.h>
 #include <mmintrin.h>
 #include <vfw.h>
 #include "Main.h"
@@ -40,6 +41,7 @@ int m_ScreenMode = 0;
 void ScreenView_CreateDisplay();
 void ScreenView_OnDraw(HDC hdc);
 //BOOL ScreenView_OnKeyEvent(WPARAM vkey, BOOL okExtKey, BOOL okPressed);
+void ScreenView_OnRButtonDown(int mousex, int mousey);
 
 const int KEYEVENT_QUEUE_SIZE = 32;
 WORD m_ScreenKeyQueue[KEYEVENT_QUEUE_SIZE];
@@ -99,7 +101,7 @@ void ScreenView_CreateDisplay()
         m_hbmp = NULL;
     }
 
-    HDC hdc = GetDC(g_hwnd);
+    HDC hdc = ::GetDC(g_hwnd);
 
     m_bmpinfo.bmiHeader.biSize = sizeof( BITMAPINFOHEADER );
     m_bmpinfo.bmiHeader.biWidth = m_cxScreenWidth;
@@ -144,6 +146,9 @@ LRESULT CALLBACK ScreenViewWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARA
     UNREFERENCED_PARAMETER(lParam);
     switch (message)
     {
+    case WM_COMMAND:
+        ::PostMessage(g_hwnd, WM_COMMAND, wParam, lParam);
+        break;
     case WM_PAINT:
         {
             PAINTSTRUCT ps;
@@ -157,6 +162,9 @@ LRESULT CALLBACK ScreenViewWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARA
         break;
     case WM_LBUTTONDOWN:
         SetFocus(hWnd);
+        break;
+    case WM_RBUTTONDOWN:
+        ScreenView_OnRButtonDown(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
         break;
         //case WM_KEYDOWN:
         //    //if ((lParam & (1 << 30)) != 0)  // Auto-repeats should be ignored
@@ -178,6 +186,21 @@ LRESULT CALLBACK ScreenViewWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARA
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return (LRESULT)FALSE;
+}
+
+void ScreenView_OnRButtonDown(int mousex, int mousey)
+{
+    ::SetFocus(g_hwndScreen);
+
+    HMENU hMenu = ::CreatePopupMenu();
+    ::AppendMenu(hMenu, 0, ID_FILE_SCREENSHOT, _T("Screenshot"));
+    ::AppendMenu(hMenu, 0, ID_FILE_SCREENSHOTTOCLIPBOARD, _T("Screenshot to Clipboard"));
+
+    POINT pt = { mousex, mousey };
+    ::ClientToScreen(g_hwndScreen, &pt);
+    ::TrackPopupMenu(hMenu, 0, pt.x, pt.y, 0, g_hwndScreen, NULL);
+
+    VERIFY(::DestroyMenu(hMenu));
 }
 
 int ScreenView_GetScreenMode()
@@ -238,7 +261,7 @@ void ScreenView_RedrawScreen()
 {
     ScreenView_PrepareScreen();
 
-    HDC hdc = GetDC(g_hwndScreen);
+    HDC hdc = ::GetDC(g_hwndScreen);
     ScreenView_OnDraw(hdc);
     VERIFY(::ReleaseDC(g_hwndScreen, hdc));
 }
@@ -390,9 +413,13 @@ BOOL ScreenView_SaveScreenshot(LPCTSTR sFileName)
     LPCTSTR sFileNameExt = _tcsrchr(sFileName, _T('.'));
     BOOL result = FALSE;
     if (sFileNameExt != NULL && _tcsicmp(sFileNameExt, _T(".png")) == 0)
-        result = PngFile_SaveScreenshot((uint32_t*)pBits, colors, sFileName, m_cxScreenWidth, m_cyScreenHeight);
+        result = PngFile_SaveScreenshot(
+                (uint32_t*)pBits, colors,
+                sFileName, m_cxScreenWidth, m_cyScreenHeight);
     else
-        result = BmpFile_SaveScreenshot((uint32_t*)pBits, colors, sFileName, m_cxScreenWidth, m_cyScreenHeight);
+        result = BmpFile_SaveScreenshot(
+                (uint32_t*)pBits, colors,
+                sFileName, m_cxScreenWidth, m_cyScreenHeight);
 
     ::free(pBits);
 
